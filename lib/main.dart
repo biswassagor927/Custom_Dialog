27d99f691c4ef142dc1263aa134aced1f1dd500d
Sagor Biswas <biswassagor927@gmail.com>
2026-04-17 00:04:01 +0600

    Custom Dialog In add Some Images

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
     );
   }
@@ -17,7 +15,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState(); // ✔ Proper state creation
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -31,34 +29,59 @@ class _MyHomePageState extends State<MyHomePage> {
               context: context,
               builder: (context) {
                 return Dialog(
-                  child: Container(
-                    height: 250,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Alert Dialog",
-                            style: TextStyle(
-                              fontFamily: "Segoe UI",
-                              fontWeight: FontWeight.w700,
-                              fontSize: 20,
-                              color: Colors.black,
-                            ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    clipBehavior:
+                        Clip.none, // ✔ Allow overflow for CircleAvatar
+                    children: [
+                      Container(
+                        height: 250,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 40,
+                              ), // ✔ Increased space for avatar overlap
+                              Text(
+                                "Alert Dialog",
+                                style: TextStyle(
+                                  fontFamily: "Segoe UI",
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                "This is an alert dialog. Alert dialogs are very useful and easy to use. Users get an alert before doing any important operation.",
+                                style: TextStyle(
+                                  fontFamily: "Segoe UI",
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
                           ),
-                          Text(
-                            "This is a simple alert dialog",
-                            style: TextStyle(
-                              fontFamily: "Segoe UI",
-                              fontWeight: FontWeight.w400,
-                              fontSize: 16,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                      Positioned(
+                        top: -30,
+                        left: 0,
+                        right: 0,
+                        child: Center(
+                          child: CircleAvatar(
+                            radius: 50,
+                            backgroundImage: AssetImage('images/images.jpg'),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 );
               },
